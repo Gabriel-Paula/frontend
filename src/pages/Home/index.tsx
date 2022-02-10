9852bbd1bad865739e49c80f0bd50fd41ae7fef8
Gabriel-Paula <gabrieldepaula0512@gmail.com>
2022-02-10 13:56:14 -0300

    Mensagem

--- src/pages/Home/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-const Home = () => {
-    return (<h1>Olá</h1>)
-}
-export default Home;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,32 @@
-import { Switch, Route } from "react-router-dom"
-import { Home } from "pages"
+import { Switch, Route } from "react-router-dom";
+import {
+    Register,
+    Login,
+    AdmProject,
+    AdmProjectStore,
+    AdmFaqStore,
+    AdmFaq,
+    HomeFaq,
+    HomeProject,
+    AdmCurtidas,
+} from "pages";
+import { PrivateRoute } from "./PrivateRoute";
+
 const Routes = () => {
-    return (<Switch>
-        <Route path="/home" exact component={Home} />
-        <Route path="/" exact component={Home} />
-    </Switch>)
-}
-export default Routes
+    return (
+        <Switch>
+            <PrivateRoute path="/adm/curtidas" exact component={AdmCurtidas} />
+            <PrivateRoute path="/adm/faq/:id" exact component={AdmFaqStore} />
+            <PrivateRoute path="/adm/faq" exact component={AdmFaq} />
+            <PrivateRoute path="/adm/project/:id" exact component={AdmProjectStore} />
+            <PrivateRoute path="/adm" exact component={AdmProject} />
+
+            <Route path="/login" exact component={Login} />
+            <Route path="/register" exact component={Register} />
+            <Route path="/faq" exact component={HomeFaq} />
+            <Route path="/" exact component={HomeProject} />
+        </Switch>
+    );
+};
+
+export default Routes;
